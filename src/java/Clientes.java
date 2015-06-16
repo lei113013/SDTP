0067526fd83166a0602cfe42f03fc058522a76a1
Tiago Miranda <lei113013@estgoh.ipc.pt>
2015-06-16 15:34:06 +0100

    Classe Cliente

---
+++ src/java/Clientes.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tiago
+ */
+public class Clientes {
+    String username;
+
+    public Clientes(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "Clientes{" + "username=" + username + '}';
+    }
+    
+    
+    
+}
